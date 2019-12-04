4ed934fa30a4ce666194893a988e3a655751abef
student1311 <1311@i.ua>
2019-12-04 14:33:56 +0200

    first commit

---
+++ build/precache-manifest.7ea47529b011347145c7bee535232624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45a4a6fcaa0d90261b802ff5c1a7fc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4225e9dc085a47ca0af5",
+    "url": "/static/css/main.5156ae88.chunk.css"
+  },
+  {
+    "revision": "cc62fb7c1dd7fa437d89",
+    "url": "/static/js/2.f45db593.chunk.js"
+  },
+  {
+    "revision": "4225e9dc085a47ca0af5",
+    "url": "/static/js/main.c187b7bf.chunk.js"
+  },
+  {
+    "revision": "eb7ca9b040dbfc6c706d",
+    "url": "/static/js/runtime-main.5fefc62a.js"
+  }
+]);
